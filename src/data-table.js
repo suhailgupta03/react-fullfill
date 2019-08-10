77771870514088933557a89f19110639a3d8d1e2
Suhail Gupta <suhailgupta03@gmail.com>
2019-08-10 21:31:54 +0530

    bug(client): fix selectedRow reference when deselecting a single row

--- src/data-table.js
+++ src/data-table.js
@@ -352,6 +352,7 @@ class DataTable extends React.Component {
       // Deselected the item
       const transformedSelectedItems = { ...this.state.selectedItems };
       delete transformedSelectedItems[rowKey];
+      selectedItems = transformedSelectedItems;
       selected = false;
     } else {
       // Item needs to be selected
